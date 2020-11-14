73387a1db61d52324f4a3269e2b39dcebe8929ae
bajaco <admin@agyx.org>
2020-11-14 06:26:12 +0000

    Fix: Refactored for clarity

--- arkistak/__main__.py
+++ arkistak/__main__.py
@@ -1,36 +1,34 @@
-import sys
 import os
 import subprocess
-from .environment import VenvController
-def main():
+from .venv_controller import VenvController
+from .parse_args import parse_args
 
-    args = sys.argv[1:]
-    if len(args) > 0:
-        if args[0] == 'create':
-            if len(args) == 2:
-                venv = VenvController()
-                venv.create(args[1])
-                venv.activate()
-                subprocess.call('pip install flask', shell=True)
-            else:
-                print('Project name required.')
-        elif args[0] == 'pip':
-            if len(args) > 1:
-                venv = VenvController()
-                
-                # Returns true after activation
-                if venv.activate():
-                    subprocess.call(' '.join(args), shell=True)
-                else:
-                    print('Project must be created before using pip.')
-            else:
-                print('pip requires arguments')
-        
-        else:
-            print('Invalid Command.')
+def create(args):
+    venv = VenvController()
+    venv.create(args[1])
+    venv.activate()
+    subprocess.call('pip install flask', shell=True)
+
+def pip(args):
+    venv = VenvController()    
+            
+    # Returns true after successful activation
+    if venv.activate():
+        subprocess.call(' '.join(args), shell=True)
     else:
-        print('Invalid command')
+        print('Project must be created before using pip.')
 
-   
+
+def main():
+    args = parse_args()
+    if not args[0]:
+        print(args[1])
+    else:
+        args = args[1]
+        if args[0] == 'create':
+            create(args)
+        elif args[0] == 'pip':
+            pip(args)
+
 if __name__ == '__main__':
     main()
